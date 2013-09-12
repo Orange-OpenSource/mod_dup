29065423375d44f173507e998fbf33128976a279
Jonas Wustrack <jwustrack.ext@orange.com>
2013-09-12 16:14:03 +0200

    Also looking up httpd.h automatically with cmake.

--- cmake/Include.cmake
+++ cmake/Include.cmake
@@ -2,9 +2,10 @@ cmake_minimum_required(VERSION 2.8)
 
 find_package(Boost REQUIRED COMPONENTS thread regex)
 find_package(APR REQUIRED)
+find_package(APACHE REQUIRED)
 find_package(CURL REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIRS})
 include_directories(${APR_INCLUDE_DIR})
-include_directories(/usr/include/apache2)
+include_directories(${APACHE_INCLUDE_DIR})
 include_directories(${CURL_INCLUDE_DIR})
