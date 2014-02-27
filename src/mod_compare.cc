6fd8d5d3031f95e4a150b426d29b157ae71c2f64
dgiampaglia <dgiampaglia.ext@orange.com>
2014-02-27 09:41:20 +0100

    file opening in childInit

--- src/mod_compare.cc
+++ src/mod_compare.cc
@@ -96,9 +96,9 @@ postConfig(apr_pool_t * pPool, apr_pool_t * pLog, apr_pool_t * pTemp, server_rec
 
     ap_add_version_component(pPool, c_COMPONENT_VERSION) ;
 
-    apr_pool_cleanup_register(pPool, NULL, apr_pool_cleanup_null, closeLogFile);
+    //apr_pool_cleanup_register(pPool, NULL, apr_pool_cleanup_null, closeLogFile);
 
-    return openLogFile(gFilePath, std::ofstream::out | std::ofstream::app);;
+    return APR_SUCCESS;//openLogFile(gFilePath, std::ofstream::out | std::ofstream::app);
 }
 
 apr_status_t
@@ -124,12 +124,12 @@ apr_status_t openLogFile(const char * filepath,std::ios_base::openmode mode) {
     return APR_SUCCESS;
 }
 
-/*void
+void
 childInit(apr_pool_t *pPool, server_rec *pServer)
 {
-    //gFile.open(gFilePath, std::ofstream::out | std::ofstream::app );
-    //apr_pool_cleanup_register(pPool, NULL, apr_pool_cleanup_null, closeLogFile);
-}*/
+    gFile.open(gFilePath, std::ofstream::out | std::ofstream::app );
+    apr_pool_cleanup_register(pPool, NULL, apr_pool_cleanup_null, closeLogFile);
+}
 
 /**
  * @brief Set the list of errors which stop the comparison
@@ -286,7 +286,7 @@ void
 registerHooks(apr_pool_t *pPool) {
 #ifndef UNIT_TESTING
     ap_hook_post_config(postConfig, NULL, NULL, APR_HOOK_MIDDLE);
-    //ap_hook_child_init(&childInit, NULL, NULL, APR_HOOK_MIDDLE);
+    ap_hook_child_init(&childInit, NULL, NULL, APR_HOOK_MIDDLE);
     ap_register_input_filter(gName, inputFilterHandler, NULL, AP_FTYPE_RESOURCE);
     ap_register_output_filter(gName, outputFilterHandler, NULL, AP_FTYPE_RESOURCE);
 #endif
