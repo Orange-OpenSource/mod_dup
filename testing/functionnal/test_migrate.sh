4d35a44f6e474f26effe6591f9a5414497d79e85
Mathieu Lornac <mlornac.ext@orange.com>
2014-08-19 12:03:41 +0200

    Adding global testing target

--- testing/functionnal/test_migrate.sh
+++ testing/functionnal/test_migrate.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-rm -f outjmeter
-jmeter -n -l outjmeter.tmp -t $1
+rm -f $1/outjmeter
+jmeter -n -l $1/outjmeter.tmp -t $1/data/migrate/test_migrate.jmx
 
-linecount=`cat outjmeter.tmp | grep ',false' | wc -l`
-
-# restore backup of migrate.conf
-if [ -e /etc/apache2/mods-available/migrate.conf.bak ]
-then
-	mv -fv /etc/apache2/mods-available/migrate.conf.bak /etc/apache2/mods-available/migrate.conf
-else
-	echo "" > /etc/apache2/mods-available/migrate.conf
-fi
+linecount=`cat $1/outjmeter.tmp | grep ',false' | wc -l`
 
 # check that the 6 tests in JMeter succeeded
 if [ $linecount -eq 0 ]
 then
 	# rm will fail if for any reason jmeter did not actually output to the log file
-	rm outjmeter.tmp
+	rm $1/outjmeter.tmp
 	echo -e "\n\nOK : JMeter test passed\n"
 	exit 0
 else
 	echo -e "\n\nKO : At least of the JMeter test did not succeed:\n"
-	cat outjmeter.tmp | grep ',false'
+	cat $1/outjmeter.tmp | grep ',false'
     echo -e "\n"
-	rm outjmeter.tmp
+	rm $1/outjmeter.tmp
 	exit 1
 fi
 
